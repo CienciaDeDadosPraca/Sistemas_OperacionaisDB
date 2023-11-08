8ed0f87548392344483984bf32c9acbcd2ab63e7
Bruno Petrocchi de Sena Azevedo <41311014+BrunoPtc@users.noreply.github.com>
2023-11-08 11:38:39 -0300

    Comment KeyValueDB.java

--- code/KeyValueDB.java
+++ code/KeyValueDB.java
@@ -6,104 +6,107 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class KeyValueDB {
-    private static final String FILE_NAME = "Database.txt";
+
+    private static final String FILE_NAME = "Database.txt"; // Define o nome do arquivo para armazenamento do banco de dados
 
     public static void main(String[] args) {
-        KeyValueDatabase keyValueDB = new KeyValueDatabase(FILE_NAME);
+        // Analisa os argumentos da linha de comando
         String command = args[0];
         String[] keyValue = args[1].split(",");
-        
 
+        KeyValueDatabase keyValueDB = new KeyValueDatabase(FILE_NAME); // Inicializa o objeto KeyValueDatabase
 
-            if (command.equals("inserir") || command.equals("remover") || command.equals("buscar") || command.equals("atualizar")) {
-                if (keyValue.length != 2) {
-                    System.out.println("Formato inválido. Use o formato 'chave,valor'.");
-                }
-            
-                int key = Integer.parseInt(keyValue[0]);
-                
-                try {
-                    if (command.equals("inserir")) {
+        // Lida com diferentes operações de banco de dados com base no comando
+        if (command.equals("inserir") || command.equals("remover") || command.equals("buscar") || command.equals("atualizar")) {
+            if (keyValue.length != 2) { // Valida o número de pares de valores-chave
+                System.out.println("Formato inválido. Use o formato 'chave,valor'.");
+            } else {
+                int key = Integer.parseInt(keyValue[0]); // Extrai a chave da entrada
+
+                try { // Lida com possíveis exceções de E/S
+                    if (command.equals("inserir")) { // Insire um novo par de valores-chave
                         String value = keyValue[1];
                         keyValueDB.insert(key, value);
-                    } else if (command.equals("remover")) {
+                    } else if (command.equals("remover")) { // Remove um par de valores-chave
                         keyValueDB.remove(key);
-                    } else if (command.equals("buscar")) {
+                    } else if (command.equals("buscar")) { // Procura um valor com base na chave
                         String result = keyValueDB.search(key);
                         System.out.println("Valor encontrado: " + result);
-                    } else if (command.equals("atualizar")) {
+                    } else if (command.equals("atualizar")) { // Atualiza o valor de uma chave existente
                         String value = keyValue[1];
                         keyValueDB.update(key, value);
                     }
                 } catch (IOException e) {
-                    System.out.println("Erro ao acessar o arquivo: " + e.getMessage());
+                    System.out.println("Erro ao acessar o arquivo: " + e.getMessage()); // Lida com erros de IO
                 } catch (NumberFormatException e) {
-                    System.out.println("O valor de entrada para a chave deve ser um número inteiro.");
+                    System.out.println("O valor de entrada para a chave deve ser um número inteiro."); // Lida com formato de chave inválido
                 }
-            } else {
-                System.out.println("Comando desconhecido: " + command);
             }
+        } else {
+            System.out.println("Comando desconhecido: " + command); // Lida com comandos inválidos
+        }
     }
 }
 
 class KeyValueDatabase {
-    private String fileName;
-    private HashMap<Integer, String> data;
+
+    private String fileName; // Armazena o nome do arquivo do banco de dados
+    private HashMap<Integer, String> data; // Armazena os pares chave-valor em um HashMap
 
     public KeyValueDatabase(String fileName) {
         this.fileName = fileName;
-        this.data = new HashMap<>();
-        loadFromFile();
+        this.data = new HashMap<>(); // Inicializa o HashMap
+        loadFromFile(); // Carrega os dados do banco de dados do arquivo
     }
 
     public void insert(int key, String value) throws IOException {
-        data.put(key, value);
-        saveToFile();
-        System.out.println("Inserido.");
+        data.put(key, value); // Insire o par chave-valor no HashMap
+        saveToFile(); // Salva os dados atualizados no arquivo
+        System.out.println("Inserido."); // Imprime mensagem de sucesso
     }
 
     public void remove(int key) throws IOException {
-        data.remove(key);
-        saveToFile();
-        System.out.println("Removido.");
+        data.remove(key); // Remove o par chave-valor do HashMap
+        saveToFile(); // Salva os dados atualizados no arquivo
+        System.out.println("Removido."); // Imprime mensagem de sucesso
     }
 
     public String search(int key) {
-        return data.get(key);
+        return data.get(key); // Retorna o valor associado à chave
     }
 
     public void update(int key, String newValue) throws IOException {
-        if (data.containsKey(key)) {
-            data.put(key, newValue);
-            saveToFile();
-            System.out.println("Atualizado.");
+        if (data.containsKey(key)) { // Verifica se a chave existe
+            data.put(key, newValue); // Atualiza o valor da chave existente
+            saveToFile(); // Salva os dados atualizados no arquivo
+            System.out.println("Atualizado."); // Imprime mensagem de sucesso
         } else {
-            System.out.println("Chave não encontrada.");
+            System.out.println("Chave não encontrada."); // Imprime mensagem de chave não encontrada
         }
     }
 
     private void loadFromFile() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) { // Lê o arquivo do banco de dados
             String line;
-            while ((line = reader.readLine()) != null) {
-                String[] keyValue = line.split(",");
-                if (keyValue.length == 2) {
-                    int key = Integer.parseInt(keyValue[0]);
-                    String value = keyValue[1];
-                    data.put(key, value);
+            while ((line = reader.readLine()) != null) { // Lê cada linha
+                String[] keyValue = line.split(","); // Divide a linha em chave e valor
+                if (keyValue.length == 2) { // Verifica se a linha tem um formato válido
+                    int key = Integer.parseInt(keyValue[0]); // Converte chave em número inteiro
+                    String value = keyValue[1]; // Extrai valor
+                    data.put(key, value); // Adiciona par chave-valor ao HashMap
                 }
             }
         } catch (IOException e) {
-            System.out.println("Erro ao carregar o arquivo: " + e.getMessage());
+            System.out.println("Erro ao carregar o arquivo: " + e.getMessage()); // Lida com erros de carregamento de arquivos
         }
     }
 
     private void saveToFile() throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            for (Integer key : data.keySet()) {
-                String value = data.get(key);
-                writer.write(key + "," + value);
-                writer.newLine();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) { // Reescreve o arquivo de banco de dados
+            for (Integer key : data.keySet()) { // Itera sobre chaves
+                String value = data.get(key); // Pega o valor associado
+                writer.write(key + "," + value); // Grava par de valores-chave no arquivo
+                writer.newLine(); // Insire uma nova linha após cada entrada
             }
         }
     }
