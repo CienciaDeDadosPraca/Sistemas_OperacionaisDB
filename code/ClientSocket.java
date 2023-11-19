98413b83f360dd5c73b7ca58ede7d653608a78c4
Bruno Petrocchi de Sena Azevedo <41311014+BrunoPtc@users.noreply.github.com>
2023-11-19 20:54:36 -0300

    Update Comments on ClientSocket.java

--- code/ClientSocket.java
+++ code/ClientSocket.java
@@ -1,81 +1,58 @@
-
-import java.io.*;
-import java.net.Socket;
-import java.net.SocketAddress;
-
-/**
- * Permite enviar e receber mensagens por meio de um socket cliente.
- * Tal classe é utilizada tanto pela aplicação cliente {@link BlockingChatClientApp}
- * quanto pelo servidor {@link BlockingChatServerApp}.
- *
- * <p>O servidor cria uma instância desta classe para cada cliente conectado,
- * assim ele pode mensagens para e receber mensagens de cada cliente.
- * Cada cliente que conecta no servidor também cria uma instância dessa classe,
- * assim ele pode enviar para e receber mensagens do servidor.</p>
- *
- * @author Manoel Campos da Silva Filho
- */
+/*
+ Permite enviar e receber mensagens por meio de um socket cliente.
+ Tal classe é utilizada tanto pela aplicação cliente {@link BlockingChatClientApp} quanto pelo servidor {@link BlockingChatServerApp}.
+ O servidor cria uma instância desta classe para cada cliente conectado, assim ele pode enviar mensagens para e receber mensagens de cada cliente.
+ Cada cliente que conecta no servidor também cria uma instância dessa classe, assim ele pode enviar para e receber mensagens do servidor.
+*/
 public class ClientSocket implements Closeable {
-    /**
-     * Login do cliente.
-     */
+    // Login do cliente.
     private String login;
 
-    /**
-     * Socket representando a conexão de um cliente com o servidor.
-     */
+    // Socket representando a conexão de um cliente com o servidor.
     private final Socket socket;
 
-    /**
-     *  Permite ler mensagens recebidas ou enviadas pelo cliente.
-     *  Se o {@link ClientSocket} foi criado pela aplicação {@link BlockingChatServerApp}, tal atributo permite ao {@link BlockingChatServerApp}
-     *  ler mensagens enviadas pelo cliente.
-     *  Se o {@link ClientSocket} foi criado pela aplicação {@link BlockingChatClientApp}, tal atributo
-     *  permite ao {@link BlockingChatClientApp} ler mensagens enviadas pelo servidor.
-     */
+    /*
+     Permite ler mensagens recebidas ou enviadas pelo cliente.
+     Se o {@link ClientSocket} foi criado pela aplicação {@link BlockingChatServerApp}, tal atributo permite ao {@link BlockingChatServerApp} ler mensagens enviadas pelo cliente.
+     Se o {@link ClientSocket} foi criado pela aplicação {@link BlockingChatClientApp}, tal atributo permite ao {@link BlockingChatClientApp} ler mensagens enviadas pelo servidor.
+    */
     private final BufferedReader in;
 
-    /**
-     *  Permite enviar mensagens do cliente para o servidor ou do servidor para o cliente.
-     *  Se o {@link ClientSocket} foi criado pela aplicação {@link BlockingChatServerApp}, tal atributo permite ao {@link BlockingChatServerApp}
-     *  enviar mensagens ao cliente.
-     *  Se o {@link ClientSocket} foi criado pela aplicação {@link BlockingChatClientApp}, tal atributo
-     *  permite ao {@link BlockingChatClientApp} enviar mensagens ao servidor.
-     */
+    /*
+     Permite enviar mensagens do cliente para o servidor ou do servidor para o cliente.
+     Se o {@link ClientSocket} foi criado pela aplicação {@link BlockingChatServerApp}, tal atributo permite ao {@link BlockingChatServerApp} enviar mensagens ao cliente.
+     Se o {@link ClientSocket} foi criado pela aplicação {@link BlockingChatClientApp}, tal atributo permite ao {@link BlockingChatClientApp} enviar mensagens ao servidor.
+    */
     private final PrintWriter out;
 
-    /**
-     * Instancia um ClientSocket.
-     *
-     * @param socket socket que representa a conexão do cliente com o servidor.
-     * @throws IOException quando não for possível obter
-     *         os objetos {@link #in} ou {@link #out} que permitem,
-     *         respectivamente, receber e enviar mensagens pelo socket.
-     *         Tal erro pode ocorrer quando, por exemplo, a conexão com o servidor cair
-     *         por falha do lado do servidor o do cliente.
-     */
+    /*
+     Instancia um ClientSocket.
+     @param socket socket que representa a conexão do cliente com o servidor.
+     @throws IOException quando não for possível obter os objetos {@link #in} ou {@link #out} que permitem, respectivamente, receber e enviar mensagens pelo socket.
+     Tal erro pode ocorrer quando, por exemplo, a conexão com o servidor cair por falha do lado do servidor o do cliente.
+    */
     public ClientSocket(final Socket socket) throws IOException {
         this.socket = socket;
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.out = new PrintWriter(socket.getOutputStream(), true);
     }
 
-    /**
-     * Envia uma mensagem e <b>não</b> espera por uma resposta.
-     * @param msg mensagem a ser enviada
-     * @return true se o socket ainda estava aberto e a mensagem foi enviada, false caso contrário
-     */
+    /*
+     Envia uma mensagem e <b>não</b> espera por uma resposta.
+     @param msg mensagem a ser enviada
+     @return true se o socket ainda estava aberto e a mensagem foi enviada, false caso contrário
+    */
     public boolean sendMsg(String msg) {
         out.println(msg);
         
-        //retorna true se não houve nenhum erro ao enviar mensagem ou false caso tenha havido
+        // Retorna true se não houve nenhum erro ao enviar mensagem ou false caso tenha havido
         return !out.checkError();
     }
 
-    /**
-     * Obtém uma mensagem de resposta.
-     * @return a mensagem obtida ou null se ocorreu erro ao obter a resposta (como falha de conexão)
-     */
+    /*
+     Obtém uma mensagem de resposta.
+     @return a mensagem obtida ou null se ocorreu erro ao obter a resposta (como falha de conexão)
+    */
     public String getMessage() {
         try {
             return in.readLine();
@@ -92,9 +69,7 @@ public class ClientSocket implements Closeable {
         return login;
     }
 
-    /**
-     * Fecha a conexão do socket e os objetos usados para enviar e receber mensagens.
-     */
+    // Fecha a conexão do socket e os objetos usados para enviar e receber mensagens.
     @Override
     public void close() {
         try {
