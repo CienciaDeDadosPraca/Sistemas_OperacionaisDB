caa4ec0fc14f978b765d69c57110f65d92bbcf2f
Bruno Petrocchi de Sena Azevedo <41311014+BrunoPtc@users.noreply.github.com>
2023-11-19 20:58:59 -0300

    Update Comments on KeyValueDB.java

--- code/KeyValueDB.java
+++ code/KeyValueDB.java
@@ -5,10 +5,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 
+// Classe que implementa um banco de dados chave-valor persistente em um arquivo.
 public class KeyValueDB {
 
     private static final String FILE_NAME = "Database.txt"; // Define o nome do arquivo para armazenamento do banco de dados
 
+    // Método principal que interpreta os argumentos da linha de comando e realiza operações no banco de dados.
     public static void main(String[] args) {
         // Analisa os argumentos da linha de comando
         String command = args[0];
@@ -48,33 +50,39 @@ public class KeyValueDB {
     }
 }
 
+// Classe que representa um banco de dados chave-valor.
 class KeyValueDatabase {
 
     private String fileName; // Armazena o nome do arquivo do banco de dados
     private HashMap<Integer, String> data; // Armazena os pares chave-valor em um HashMap
 
+    // Construtor que inicializa o banco de dados com o nome do arquivo especificado.
     public KeyValueDatabase(String fileName) {
         this.fileName = fileName;
         this.data = new HashMap<>(); // Inicializa o HashMap
         loadFromFile(); // Carrega os dados do banco de dados do arquivo
     }
 
+    // Insere um novo par chave-valor no banco de dados.
     public void insert(int key, String value) throws IOException {
         data.put(key, value); // Insire o par chave-valor no HashMap
         saveToFile(); // Salva os dados atualizados no arquivo
         System.out.println("Inserido."); // Imprime mensagem de sucesso
     }
 
+    // Remove um par chave-valor do banco de dados.
     public void remove(int key) throws IOException {
         data.remove(key); // Remove o par chave-valor do HashMap
         saveToFile(); // Salva os dados atualizados no arquivo
         System.out.println("Removido."); // Imprime mensagem de sucesso
     }
 
+    // Procura um valor associado à chave no banco de dados.
     public String search(int key) {
         return data.get(key); // Retorna o valor associado à chave
     }
 
+    // Atualiza o valor associado à chave no banco de dados.
     public void update(int key, String newValue) throws IOException {
         if (data.containsKey(key)) { // Verifica se a chave existe
             data.put(key, newValue); // Atualiza o valor da chave existente
@@ -85,6 +93,7 @@ class KeyValueDatabase {
         }
     }
 
+    // Carrega os dados do banco de dados a partir do arquivo.
     private void loadFromFile() {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) { // Lê o arquivo do banco de dados
             String line;
@@ -101,6 +110,7 @@ class KeyValueDatabase {
         }
     }
 
+    // Salva os dados do banco de dados no arquivo.
     private void saveToFile() throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) { // Reescreve o arquivo de banco de dados
             for (Integer key : data.keySet()) { // Itera sobre chaves
