6b42448c1590c08964bfe52e5667c54a3d146309
MeloRPN <102264026+MeloRPN@users.noreply.github.com>
2023-12-10 19:38:13 -0300

    Update FIFO.java

--- code/FIFO.java
+++ code/FIFO.java
@@ -5,14 +5,32 @@ import java.util.Queue;
   
 class FIFO
 { 
-    // Method to find page faults using FIFO 
+    //Método para encontrar falhas de página usando FIFO
     static int pageFaults(int pages[], int n, int capacity) 
     { 
-        // To represent set of current pages. We use 
-        // an unordered_set so that we quickly check 
-        // if a page is present in set or not 
+        // Para representar o conjunto de páginas atuais. É utilizado
+        // um conjunto não ordenado para verificar rapidamente se a página está presente no conjunto ou não.
         HashSet<Integer> s = new HashSet<>(capacity); 
        
-        // To store the pages in FIFO manner 
+        // Guardar as páginas em FIFO
         Queue<Integer> indexes = new LinkedList<>() ; 
+        
+        // Início da página inicial 
+        int page_faults = 0; 
+        for (int i=0; i<n; i++) 
+        { 
+            // Checar se cabe mais páginas no conjunto 
+            if (s.size() < capacity) 
+            { 
+                // Inserção de página no conjunto 
+                if (!s.contains(pages[i])) 
+                { 
+                    s.add(pages[i]); 
        
+                    // Incremento na falha de página
+                    page_faults++; 
+       
+                    // Empurrar a página corrente na fila
+                    indexes.add(pages[i]); 
+                } 
+            }  
