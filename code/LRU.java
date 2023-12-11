e182d3f80ff8c48dc304879557869afb6d2974cd
Bruno Petrocchi de Sena Azevedo <41311014+BrunoPtc@users.noreply.github.com>
2023-12-10 21:45:13 -0300

    Update LRU.java

--- code/LRU.java
+++ code/LRU.java
@@ -4,50 +4,42 @@ import java.util.Iterator;
   
 class LRU
 { 
-    // Método para encontrar falhas de página usando índices 
+    // Método para encontrar falhas de página usando índices
     static int pageFaults(int pages[], int n, int capacity) 
     { 
-        // Para representar o conjunto de páginas atuais. Usamos
-        // um unordered_set para que possamos verificar rapidamente
-        // se uma página está presente no conjunto ou não
+        // Para representar o conjunto de páginas atuais. Usamos um unordered_set para que possamos verificar rapidamente se uma página está presente no conjunto ou não
         HashSet<Integer> s = new HashSet<>(capacity); 
        
-        // Para armazenar índices usados ​​recentemente
-        // de páginas
+        // Para armazenar índices usados ​​recentemente de páginas
         HashMap<Integer, Integer> indexes = new HashMap<>(); 
        
-        // Comece da página inicial
+        // Começa da página inicial
         int page_faults = 0; 
         for (int i=0; i<n; i++) 
         { 
-            // Verifique se o conjunto pode conter mais páginas
+            // Verifica se o conjunto pode conter mais páginas
             if (s.size() < capacity) 
             { 
-                // Insira-o no conjunto se não estiver presente
-                // já que representa falha de página
+                // Insire no conjunto se não estiver presente já que representa falha de página
                 if (!s.contains(pages[i])) 
                 { 
                     s.add(pages[i]); 
        
-                    // incrementa a falha de página
+                    // Incrementa a falha de página
                     page_faults++; 
                 } 
        
-                // Armazene o índice usado recentemente de
-                // cada página
+                // Armazena o índice usado recentemente de cada página
                 indexes.put(pages[i], i); 
             } 
        
-            // Se o conjunto estiver cheio, então é necessário realizar lru
-            // ou seja, remova a página menos usada recentemente
-            // e insira a página atual
+            // Se o conjunto estiver cheio, então é necessário realizar LRU ou seja, remover a página menos usada recentemente e insirir a página atual
             else
             { 
                 
                 if (!s.contains(pages[i])) 
                 { 
-                    // Encontre as páginas menos usadas recentemente
-                    // que está presente no set
+                    // Encontra as páginas menos usadas recentemente que está presente no set
                     int lru = Integer.MAX_VALUE, val=Integer.MIN_VALUE; 
                       
                     Iterator<Integer> itr = s.iterator(); 
@@ -61,18 +53,20 @@ class LRU
                         } 
                     } 
                   
-                    // Remova a página de índices
-                    s.remove(val); 
-                   //remove lru do hashmap
-                   indexes.remove(val); 
-                    // insira a página atual
+                    // Remove a página de índices
+                    s.remove(val);
+                  
+                    // Remove LRU do hashmap
+                    indexes.remove(val);
+                  
+                    // Insire a página atual
                     s.add(pages[i]); 
        
                     // Incrementa falhas de página 
                     page_faults++; 
                 } 
        
-                // Atualize o índice da página atual
+                // Atualiza o índice da página atual
                 indexes.put(pages[i], i); 
             } 
         } 
