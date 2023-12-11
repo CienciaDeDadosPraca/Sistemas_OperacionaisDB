b2d74a61984b26a0d91b4ff26508fcc44adde476
Bruno Petrocchi de Sena Azevedo <41311014+BrunoPtc@users.noreply.github.com>
2023-12-10 21:55:05 -0300

    Update FIFO.java

--- code/FIFO.java
+++ code/FIFO.java
@@ -4,33 +4,53 @@ import java.util.Queue;
   
   
 class FIFO
-{ 
-    //Método para encontrar falhas de página usando FIFO
-    static int pageFaults(int pages[], int n, int capacity) 
-    { 
-        // Para representar o conjunto de páginas atuais. É utilizado
-        // um conjunto não ordenado para verificar rapidamente se a página está presente no conjunto ou não.
-        HashSet<Integer> s = new HashSet<>(capacity); 
+{
+    // Método para encontrar falhas de página usando FIFO
+    static int pageFaults(int pages[], int n, int capacity)
+    {
+        // Para representar o conjunto de páginas atuais. É utilizado um conjunto não ordenado para verificar rapidamente se a página está presente no conjunto ou não.
+        HashSet<Integer> s = new HashSet<>(capacity);
        
-        // Guardar as páginas em FIFO
-        Queue<Integer> indexes = new LinkedList<>() ; 
+        // Guarda as páginas em FIFO
+        Queue<Integer> indexes = new LinkedList<>();
         
         // Início da página inicial 
-        int page_faults = 0; 
-        for (int i=0; i<n; i++) 
-        { 
-            // Checar se cabe mais páginas no conjunto 
-            if (s.size() < capacity) 
-            { 
-                // Inserção de página no conjunto 
-                if (!s.contains(pages[i])) 
-                { 
-                    s.add(pages[i]); 
-       
-                    // Incremento na falha de página
-                    page_faults++; 
-       
-                    // Empurrar a página corrente na fila
-                    indexes.add(pages[i]); 
-                } 
-            }  
+        int page_faults = 0;
+        for (int i=0; i<n; i++)
+        {
+            // Confere se cabe mais páginas no conjunto 
+            if (s.size() < capacity)
+            {
+                // Insere página no conjunto 
+                if (!s.contains(pages[i]))
+                {
+                    s.add(pages[i]);
+                    // Incrementa falha de página
+                    page_faults++;
+                    // Empurra a página corrente na fila
+                    indexes.add(pages[i]);
+                }
+            }
+            else
+            {
+                // Confere se a página atual não está já presente
+                if(!s.contains(pages[i]))
+                {
+                    // Adiciona a primeira página da fila
+                    in val = indexes.peek();
+                    indexes.poll();
+                    // Remove a página de indices
+                    s.remove(val);
+                    // Insere a página atual
+                    s.add(pages[i]);
+                    // Avança a página na fila
+                    indexes.add(pages[i]);
+                    // Incrementa falha de página
+                    page_faults++;
+                }
+            }
+        }
+
+        return page_faults;
+    }
+}
